807541aeb1614f02eb50bb5efb543c9bce353814
Francisco Souza <fss@corp.globo.com>
2014-03-30 19:50:14 -0300

    server: define some flags

--- server.go
+++ server.go
@@ -5,14 +5,37 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/globocom/tsuru/db/storage"
 )
 
-var databaseAddr, databaseName string
+var (
+	databaseAddr string
+	databaseName string
+	baseDir      string
+	readHttp     string
+	writeHttp    string
+)
+
+func init() {
+	flag.StringVar(&databaseAddr, "mongodb", "127.0.0.1:27017", "Address of the database server")
+	flag.StringVar(&databaseName, "dbname", "archives", "Name of the database to store information about archives")
+	flag.StringVar(&baseDir, "dir", "/var/lib/archives/", "Base directory, where the server will create and serve the archives")
+	flag.StringVar(&readHttp, "read-http", "", "Address to bind the API that serves archives. Omit to not start this API.")
+	flag.StringVar(&writeHttp, "write-http", "", "Address to bind the API that creates archives. Omit to not start this API.")
+}
 
 func conn() (*storage.Storage, error) {
 	return storage.Open(databaseAddr, databaseName)
 }
 
 func main() {
+	flag.Parse()
+	if readHttp == "" && writeHttp == "" {
+		fmt.Println("You need to specify at-least one of -read-http and -write-http")
+		os.Exit(1)
+	}
 }
