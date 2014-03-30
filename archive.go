26fe6f6f9529de046a289ea542aeb8ef3284eacd
Francisco Souza <fss@corp.globo.com>
2014-03-30 19:00:13 -0300

    archive: add function to create a new archive

--- archive.go
+++ archive.go
@@ -4,6 +4,19 @@
 
 package main
 
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha512"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"labix.org/v2/mgo/bson"
+)
+
 const (
 	StatusBuilding Status = iota
 	StatusReady
@@ -29,4 +42,64 @@ type Archive struct {
 	ID     string `bson:"_id"`
 	Path   string
 	Status Status
+	Log    string
+}
+
+func NewArchive(path, refid, baseDir, prefix string) (*Archive, error) {
+	archive := Archive{
+		ID:     newID(path),
+		Status: StatusBuilding,
+	}
+	archive.Path = filepath.Join(baseDir, archive.ID+".tar.gz")
+	db, err := conn()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	err = db.Collection("archives").Insert(archive)
+	if err != nil {
+		return nil, err
+	}
+	go generate(archive, path, refid, archive.Path, prefix)
+	return &archive, nil
+}
+
+func newID(path string) string {
+	var buf [32]byte
+	_, err := rand.Read(buf[:])
+	if err != nil {
+		return ""
+	}
+	nanoUnix := time.Now().UnixNano()
+	hash := sha512.New()
+	hash.Write(buf[:])
+	hash.Write([]byte(path))
+	hash.Write([]byte(fmt.Sprintf("%d", nanoUnix)))
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func generate(archive Archive, repositoryPath, refid, archivePath, prefix string) {
+	db, err := conn()
+	if err != nil {
+		return
+	}
+	defer db.Close()
+	status := StatusReady
+	if !strings.HasSuffix(prefix, "/") {
+		prefix += "/"
+	}
+	var buf bytes.Buffer
+	command := exec.Command(
+		"git", "archive", "--format=tar.gz",
+		"--output="+archivePath, "--prefix="+prefix, refid,
+	)
+	command.Dir = repositoryPath
+	command.Stdout = &buf
+	command.Stderr = &buf
+	if err := command.Run(); err != nil {
+		status = StatusError
+	}
+	archive.Log = buf.String()
+	update := bson.M{"$set": bson.M{"status": status, "log": archive.Log}}
+	db.Collection("archives").UpdateId(archive.ID, update)
 }
--- archive_test.go
+++ archive_test.go
@@ -5,6 +5,11 @@
 package main
 
 import (
+	"path/filepath"
+	"time"
+
+	"github.com/tsuru/commandmocker"
+	"labix.org/v2/mgo/bson"
 	"launchpad.net/gocheck"
 )
 
@@ -21,5 +26,80 @@ func (Suite) TestStatus(c *gocheck.C) {
 	for _, t := range tests {
 		got := t.input.String()
 		c.Check(got, gocheck.Equals, t.expected)
+	}
+}
+
+func (Suite) TestNewArchive(c *gocheck.C) {
+	tmpdir, err := commandmocker.Add("git", "success")
+	c.Assert(err, gocheck.IsNil)
+	defer commandmocker.Remove(tmpdir)
+	path, _ := filepath.Abs("testdata/test.git")
+	archive, err := NewArchive(path, "e101294022323", "/tmp/archive-server", "sproject")
+	c.Assert(err, gocheck.IsNil)
+	sess, err := conn()
+	c.Assert(err, gocheck.IsNil)
+	defer sess.Close()
+	defer sess.Collection("archives").RemoveId(archive.ID)
+	c.Assert(archive.Status, gocheck.Equals, StatusBuilding)
+	c.Assert(archive.Path, gocheck.Equals, "/tmp/archive-server/"+archive.ID+".tar.gz")
+	wait(c, 3e9, func() bool {
+		count, err := sess.Collection("archives").Find(bson.M{"_id": archive.ID, "status": StatusReady}).Count()
+		return err == nil && count == 1
+	})
+	c.Assert(commandmocker.Ran(tmpdir), gocheck.Equals, true)
+	expected := []string{
+		"archive", "--format=tar.gz",
+		"--output=/tmp/archive-server/" + archive.ID + ".tar.gz",
+		"--prefix=sproject/", "e101294022323",
+	}
+	c.Assert(commandmocker.Parameters(tmpdir), gocheck.DeepEquals, expected)
+	err = sess.Collection("archives").FindId(archive.ID).One(&archive)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(archive.Status, gocheck.Equals, StatusReady)
+	c.Assert(archive.Log, gocheck.Equals, "success")
+}
+
+func (Suite) TestNewArchiveFailure(c *gocheck.C) {
+	tmpdir, err := commandmocker.Error("git", "failed to generate file", 1)
+	c.Assert(err, gocheck.IsNil)
+	defer commandmocker.Remove(tmpdir)
+	path, _ := filepath.Abs("testdata/test.git")
+	archive, err := NewArchive(path, "e101294022323", "/tmp/archive-server", "sproject")
+	c.Assert(err, gocheck.IsNil)
+	sess, err := conn()
+	c.Assert(err, gocheck.IsNil)
+	defer sess.Close()
+	defer sess.Collection("archives").RemoveId(archive.ID)
+	wait(c, 3e9, func() bool {
+		count, err := sess.Collection("archives").Find(bson.M{"_id": archive.ID, "status": StatusError}).Count()
+		return err == nil && count == 1
+	})
+	c.Assert(commandmocker.Ran(tmpdir), gocheck.Equals, true)
+	err = sess.Collection("archives").FindId(archive.ID).One(&archive)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(archive.Status, gocheck.Equals, StatusError)
+	c.Assert(archive.Log, gocheck.Equals, "failed to generate file")
+}
+
+func wait(c *gocheck.C, timeout time.Duration, fn func() bool) {
+	done := make(chan int)
+	quit := make(chan int)
+	go func() {
+		run := true
+		for run {
+			select {
+			case <-quit:
+				run = false
+			default:
+				run = !fn()
+			}
+		}
+		close(done)
+	}()
+	select {
+	case <-done:
+	case <-time.After(timeout):
+		close(quit)
+		c.Fatalf("Timed out after %s", timeout)
 	}
 }
